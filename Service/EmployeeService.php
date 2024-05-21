bc0ae8275c59e4df31230a4fd146649292e3b174
Claire Kannapell <ckannapell7@gmail.com>
2024-05-20 22:31:02 -0500

    NCBE: create all employee types

--- Service/EmployeeService.php
+++ Service/EmployeeService.php
@@ -5,6 +5,10 @@ class EmployeeService {
     {
         return [
             new Intern(1, 'claire', $departments[0], Employee::FULL_TIME),
+            new FloorWorker(2, 'molly', $departments[1], Employee::PART_TIME),
+            new Supervisor(3, 'jamie', $departments[1], Employee::PART_TIME),
+            new Manager(4, 'sam', $departments[0], Employee::FULL_TIME),
+            new Executive(5, 'phoebe', $departments[0], Employee::FULL_TIME),
         ];
     }
 }
